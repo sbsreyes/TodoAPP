83b09040c8a68356f68963c2603336dc4da0d8b4
Sebastian Reyes <sbsreyes.js@gmail.com>
2020-08-08 22:35:53 -0500

    Se crea la funcionalidad de agregar tareas, pendiente la parte para eliminar las mismas

--- App.js
+++ App.js
@@ -1,41 +1,96 @@
+//  Componente Header
 Vue.component('encabezado', {
     data(){
         return{
             bootsTrapClasses:{
                 title: 'Sebastians To Do App',
-                estiloBg:'bg-success',
                 estiloTexto:'text-light'
             }          
         }
     },
     template:`
     <div class="row">
-        <div class="col-sm-12 mt-5" v-bind:class="[bootsTrapClasses.estiloTexto, bootsTrapClasses.estiloBg]">
+        <div class="col-sm-12 mt-3" v-bind:class="bootsTrapClasses.estiloTexto">
             <h1>{{ bootsTrapClasses.title }}</h1>
         </div>
     </div>
     `
 })
 
+// Componente Tarea
 Vue.component('task', {
+    props: [
+        'fila',
+        'columna'
+    ],
     data(){
         return{
-
+            tasks:[],
+            taskName:''
         }
     },
-    template: `
-    <div class="card mt-5">
-        <div class="card-body">
-        This is some text within a card body.
+    template:
+    `
+        <div>
+            <div class="card mt-5" v-for="t in tasks">
+                <div class="row align-items-center">
+                    <div class="col-sm-6">
+                        <h5 style="padding: 25px;">{{t}}</h5>
+                    </div>
+                    
+                    <div class="col-sm-6 ">
+                        <a href="#" class="btn btn-danger">Eliminar tarea</a>
+                    </div>
+                </div>
+            </div>
+            <button type="button" class="btn btn-success btn-block mt-3" data-toggle="modal" data-target="#exampleModal">
+            Crear una nueva tarea
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <input class="form-control" v-model="taskName" placeholder="Escribe la tarea" type="text">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-warning" data-dismiss="modal" v-on:click="addTask">Agregar Tarea</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>`
+        
+    `,
+    watch: {
+        taskName(value){
+            console.log(value)
+        }
+    },
+    methods:{
+        addTask(){
+            this.tasks.push(this.taskName)
+            console.log(this.tasks)
+            this.taskName = ''
+        }
+    }
 })
 
+// Componente Padre
 new Vue({
-   el:'#app',
-   data(){
-       return{
-           title:'Trabajando con vue y bootstrap'
-       }
-   },
-})
+    el:'#app',
+    data(){
+        return{
+            fila:'row',
+            columna:'col'
+        }
+    },
+ })
--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # TodoAPP
 Está aplicación es una puesta en práctica de lo aprendido del Framework VueJs
 
-### Una breve descripcion del proyecto:
+### Una breve descripción del proyecto:
 
 La intención de este proyecto es aplicar a nivel básico el uso de JavaScrips a través del CDN de vue, al igual que vamos a utilizar los estilos
 de BOOTSTRAP, para facilitar un poco el desarrollo frontend
+
+# Vista principal de la aplicación:
+
+![image-20200808223102036](Img/Diseño de la app.png)
+
+
+
+# Agregando una nueva tarea:
+
+![image-20200808223102036](Img/Agregando una tarea.png)
+
+Ya con esto damos por terminada el ejercicio de práctica de VueJS
